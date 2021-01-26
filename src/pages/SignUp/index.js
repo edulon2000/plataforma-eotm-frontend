b8fc5a741539087367b394fee831af63640ef726
edulon2000 <edulon2015@gmail.com>
2021-01-26 15:55:44 -0300

    dev pag de cadastro

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
-import { Form, Container } from "./styles";
+import { Form, Container} from "./styles";
 
 class SignUp extends Component {
   state = {
--- src/pages/SignUp/styles.js
+++ src/pages/SignUp/styles.js
@@ -5,10 +5,25 @@ export const Container = styled.div`
   align-items: center;
   justify-content: center;
   height: 100vh;
+  background: url("assets/imagemdefundo.png");
+  mix-blend-mode: luminosity;
+  img{
+        position: absolute;
+    width: 83px;
+    height: 83px;
+    left: 124px;
+    top: 17px;
+
+    background: url('assets/eotm.png');
+    border-radius: 150px;
+
+  }
 `;
 
 export const Form = styled.form`
-  width: 400px;
+  width: 420px;
+  box-shadow: 0px 9px 29px rgba(0, 0, 0, 0.25);
+  border-radius: 25px;
   background: #fff;
   padding: 20px;
   display: flex;
@@ -33,7 +48,8 @@ export const Form = styled.form`
     padding: 0 20px;
     color: #777;
     font-size: 15px;
-    width: 100%;
+    width: 90%;
+    border-radius: 10px 10px 10px 10px;
     border: 1px solid #ddd;
     &::placeholder {
       color: #999;
@@ -42,17 +58,25 @@ export const Form = styled.form`
   button {
     color: #fff;
     font-size: 16px;
-    background: #fc6963;
+    background: #07215A;
     height: 56px;
-    border: 0;
-    border-radius: 5px;
+    border: none;
+    border-radius: 1px;
     width: 100%;
+    -webkit-transition: all 0.5s ease-in-out;
+    -moz-transition: all 0.5s ease-in-out;
+    -ms-transition: all 0.5s ease-in-out;
+    -o-transition: all 0.5s ease-in-out;
+    transition: all 0.5s ease-in-out;
+    -webkit-border-radius: 5px 5px 5px 5px;
+    border-radius: 5px 5px 5px 5px;
   }
   hr {
     margin: 20px 0;
     border: none;
     border-bottom: 1px solid #cdcdcd;
     width: 100%;
+    
   }
   a {
     font-size: 16;
@@ -60,4 +84,5 @@ export const Form = styled.form`
     color: #999;
     text-decoration: none;
   }
-`;
+  
+`
